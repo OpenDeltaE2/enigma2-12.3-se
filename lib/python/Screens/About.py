847ed9674df047a0a9c8f2120e946bc4cd92fe84
Open Delta Team <86120617+OpenDelta-Team@users.noreply.github.com>
2024-06-03 02:06:33 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -35,8 +35,10 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
-		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
-		AboutText += _("Last update: 2024-03-18")  + "\n"
+		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"        
+		AboutText += _("Last update: 2024-06-03")  + "\n"
+		AboutText += _("Build Image: OpenDelta-Team") + "\n"
+		AboutText += _("Forum: https://areadeltasat.net") + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
